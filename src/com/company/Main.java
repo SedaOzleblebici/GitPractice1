ef9cad5cfd54fcf4f6ea74497f96d6cb763d707f
sedaa <seda.ozleblebi@gmail.com>
2023-01-16 02:48:47 +0000

    ıkıncı degısıklık

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -5,6 +5,7 @@ public class Main {
     public static void main(String[] args) {
 
         System.out.println("you can");
+        System.out.println("you can do it");
 	// write your code here
     }
 }
