e63db0495e6ad782aeb2087bed668497c26ece1e
sedaa <seda.ozleblebi@gmail.com>
2023-01-24 14:54:40 +0000

    save

--- src/Test.java
+++ src/Test.java
@@ -2,6 +2,9 @@ public class Test {
     public static void main(String[] args) {
 
 //        ffds
+        System.out.println("be silent");
         System.out.println("silent");
+
+        // seda local
     }
 }
