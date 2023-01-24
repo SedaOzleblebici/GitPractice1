8f6dff8175cdcf95348895f6e8ad1a486510f12c
fEYZA lEMAN <feyzalemana@gmail.com>
2023-01-24 17:01:24 +0300

    Feyza leman commit added

--- src/FeyzaTest1.java
+++ src/FeyzaTest1.java
@@ -1,7 +1,11 @@
 public class FeyzaTest1 {
     public static void main(String[] args) {
         System.out.println("Feyza Test2 deyim");
+        System.out.println("Feyza Testdeyim");
+
+
     }
+
 
 
 
